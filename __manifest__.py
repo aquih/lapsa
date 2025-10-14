7476e845f4171ba8a7848235deff85d9b8867b23
Jefferson Silva <jefferson@computadora911.com>
2025-10-14 23:13:25 +0000

    Voucher de pago

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+# -*- encoding: utf-8 -*-
+{
+    'name': 'Inmobiliaria Lap',
+    'version': '1.0',
+    'category': 'Custom',
+    'description': """ Inmobiliaria Lap """,
+    'author': 'aquíH',
+    'website': 'http://aquih.com/',
+    'depends': ['account', 'l10n_gt_extra'],
+    'data': [
+        'views/reporte_payment1.xml',
+        'views/reporte_payment2.xml',
+        'views/reports.xml',
+    ],
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
---
+++ report/report_payment.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from odoo import api, models
+from odoo.addons.l10n_gt_extra import a_letras
+
+class ReportAbstractPayment(models.AbstractModel):
+    _name = 'inmobiliarialap.abstract.reporte_account_payment'
+
+    def totales(self, o):
+        t = {'debito': 0, 'credito': 0}
+        for l in o.move_id.line_ids:
+            t['debito'] += l.debit
+            t['credito'] += l.credit
+        return t
+
+    def a_letras(self,monto):
+        monto = round(monto, 2)
+        return a_letras.num_a_letras(monto)
+
+    def fecha_larga(self, date):
+        return str(date.day) + ' de ' + a_letras.mes_a_letras(date.month - 1) + ' de ' +  str(date.year)
+
+    def _get_report_values(self, docids, data=None):
+        model = 'account.payment'
+        docs = self.env['account.payment'].browse(docids)
+
+        return {
+            'doc_ids': docids,
+            'doc_model': model,
+            'docs': docs,
+            'data': data,
+            'fecha_larga': self.fecha_larga,
+            'a_letras': self.a_letras,
+            'totales': self.totales,
+        }
+
+class ReportPayment1(models.AbstractModel):
+    _name = 'report.inmobiliarialap.reporte_account_payment1'
+    _inherit = 'inmobiliarialap.abstract.reporte_account_payment'
+
+class ReportPayment2(models.AbstractModel):
+    _name = 'report.inmobiliarialap.reporte_account_payment2'
+    _inherit = 'inmobiliarialap.abstract.reporte_account_payment'
